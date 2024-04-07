addb3fa222b01a2ecdea77ce518c900ad2a73ef8
cockroacher <163405488+cockroacher@users.noreply.github.com>
2024-04-07 11:20:26 +0200

    prep for issue 191 in webperf_core

--- Sandboxed/Desktop/RunMeSecond.cmd
+++ Sandboxed/Desktop/RunMeSecond.cmd
@@ -1,12 +1,8 @@
 cd C:\Users\WDAGUtilityAccount\Documents\Projects\webperf_core\
 rmdir node_modules /s /q
+del package-lock.json
 pip install -r requirements.txt
-npm install pa11y
-pip install pylint
 python -m pip install --upgrade setuptools
 python -m pip install pyssim Pillow image
-
-npm install sitespeed.io
-npm install lighthouse
-npm install yellowlabtools
+npm install
 code .
