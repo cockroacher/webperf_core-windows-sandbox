23c9a82575b7a28ceec47b96b29c61effe3a2e4d
cockroacher <163405488+cockroacher@users.noreply.github.com>
2024-09-21 17:08:14 +0200

    updated deprecated npm install cmd

--- Sandboxed/Desktop/RunMeSecond.cmd
+++ Sandboxed/Desktop/RunMeSecond.cmd
@@ -4,5 +4,5 @@ del package-lock.json
 pip install -r requirements.txt
 python -m pip install --upgrade setuptools
 python -m pip install pyssim Pillow image
-npm install --production
+npm install --omit=dev
 code .
